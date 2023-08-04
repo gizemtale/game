68ae41fee767fad599205490bb085881c05cab06
gizemtale <erdugangizem@gmail.com>
2023-08-04 13:07:14 +0200

    fixed and combined

--- game.js
+++ game.js
@@ -1,71 +1,106 @@
-const choices = ["rock", "paper", "scissors"];
+// Define the list of valid moves
+const validMoves = ["rock", "paper", "scissors"];
+const maxRounds = 5;
 
-function computerPlay() {
-  const randomIndex = Math.floor(Math.random() * choices.length);
-  return choices[randomIndex];
+function getComputerSelection() {
+  const index = Math.floor(Math.random() * validMoves.length);
+  return validMoves[index];
 }
 
 function playRound(playerSelection, computerSelection) {
-  const playerSelectionLowerCase = playerSelection.toLowerCase();
-
-  if (playerSelectionLowerCase === computerSelection) {
-    return "Omg! It's a tie. Both players chose " + playerSelection + " :)";
-  } else if (
-    (playerSelectionLowerCase === "rock" && computerSelection === "scissors") ||
-    (playerSelectionLowerCase === "paper" && computerSelection === "rock") ||
-    (playerSelectionLowerCase === "scissors" && computerSelection === "paper")
+  // "0" represents tie, "1" represents win, "-1" represents lost
+  if (
+    (playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "paper" && computerSelection == "rock") ||
+    (playerSelection == "scissors" && computerSelection == "paper")
   ) {
-    return `You Win! It's a good choise! ${playerSelection} beats ${computerSelection}`;
+    return 1;
+  } else if (playerSelection === computerSelection) {
+    return 0;
   } else {
-    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    return -1;
   }
+}
+
+function getValidWordFromUser(round) {
+  let userInput;
+  do {
+    userInput = prompt(
+      `Round ${round} - Enter your selection (rock, paper, scissors):`
+    );
+
+    if (userInput === "quit" || userInput === null) {
+      return "quit";
+    }
+
+    userInput = userInput.trim().toLowerCase();
+
+    if (!validMoves.includes(userInput)) {
+      console.log(
+        `Invalid input: "${userInput}". Please enter a valid move (rock, paper, scissors).`
+      );
+    }
+  } while (!validMoves.includes(userInput));
+
+  return userInput;
 }
 
 function game() {
-  let playerScore = 0;
+  console.clear();
+  console.log(`
+***************************************************************************
+Welcome to the rock, paper, scissors game!
+You have 5 rounds to win the game. 
+To quit, press the prompt cancel button or type "quit".
+Good Luck!
+***************************************************************************
+`);
+
+  let userScore = 0;
   let computerScore = 0;
 
-  for (let round = 1; round <= 5; round++) {
-    let playerSelection = prompt(
-      `Round ${round}: Enter 'rock', 'paper', or 'scissors':`
-    );
-    if (!playerSelection) {
+  for (let round = 1; round <= maxRounds; round++) {
+    const computerSelect = getComputerSelection();
+    const validUserInput = getValidWordFromUser(round);
+
+    if (validUserInput === "quit") {
+      console.log(`>> You chose to quit. Next Hero please!`);
       return;
     }
-    while (!choices.includes(playerSelection.toLowerCase())) {
-      playerSelection = prompt(
-        `Round ${round}: Ooopss! Invalid :( Enter 'rock', 'paper', or 'scissors':`
-      );
-      if (!playerSelection) {
-        return;
-      }
-    }
 
-    const computerSelection = computerPlay();
+    const result = playRound(validUserInput, computerSelect);
 
-    const result = playRound(playerSelection, computerSelection);
-
-    console.log(result);
-
-    if (result.startsWith("You Win!")) {
-      playerScore++;
-    } else if (result.startsWith("You Lose!")) {
-      computerScore++;
+    switch (result) {
+      case -1:
+        computerScore++;
+        console.log(`>> You Lose, ${computerSelect} beats ${validUserInput}`);
+        break;
+      case 0:
+        console.log(">> It's a tie");
+        break;
+      case 1:
+        userScore++;
+        console.log(
+          `>> Congratulations! You win! ${validUserInput} beats ${computerSelect}`
+        );
+        break;
     }
   }
 
-  let winnerMessage;
-  if (playerScore > computerScore) {
-    winnerMessage = "Congratulations! You won the game!";
-  } else if (playerScore < computerScore) {
-    winnerMessage = "Sorry, you lost the game. Better luck next time!";
+  let finalResultMessage;
+  if (computerScore < userScore) {
+    finalResultMessage = `>> Congratulations,the world is saved, let's have some free food`;
+  } else if (computerScore === userScore) {
+    finalResultMessage = `>> It's a tie, Let's give it another try`;
   } else {
-    winnerMessage = "It's a tie! Both players have the same score.";
+    finalResultMessage = `>> You lost, Villain computer will take over now, Rest in peace yall`;
   }
 
-  console.log(`Final Scores: You ${playerScore} - ${computerScore} Computer`);
-  console.log(winnerMessage);
+  console.log(
+    `%cFinal Result: You ${userScore} - ${computerScore} villain AI.\n ${finalResultMessage}`,
+    "color: blue; font-size:17px"
+  );
 }
 
-const startButton = document.getElementById("startButton");
-startButton.addEventListener("click", game);
+let startGameBtn = document.querySelector(".startGame");
+startGameBtn.addEventListener("click", game);
